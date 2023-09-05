39f9f25d0568e14f39df750bac677b8763601612
thebeatles423 <144031272+thebeatles423@users.noreply.github.com>
2023-09-05 22:28:04 +0100

    PLAYBALL!

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import * as toml from 'toml';
 import IConfig from './IConfig.js';
-import * as fs from "fs";
+import * as fs from 'fs';
 import WSServer from './WSServer.js';
 import QEMUVM from './QEMUVM.js';
 import log from './log.js';
 
 log("INFO", "CollabVM Server starting up");
 
-// Parse the config file
+// Add a INFO message about the experimental feature
+log("INFO", "!!! You are using an experimental feature. Please be aware that this is not the final product. Experimental support for VNC and VMware is enabled.");
 
-var Config : IConfig;
+// Parse the config file
+var Config: IConfig;
 
 if (!fs.existsSync("config.toml")) {
-    log("FATAL", "Config.toml not found. Please copy config.example.toml and fill out fields")
+    log("FATAL", "config.toml not found. Please copy config.example.toml and fill out fields");
     process.exit(1);
 }
+
 try {
     var configRaw = fs.readFileSync("config.toml").toString();
     Config = toml.parse(configRaw);
@@ -23,14 +26,14 @@ try {
     process.exit(1);
 }
 
-
 async function start() {
     // Fire up the VM
     var VM = new QEMUVM(Config);
     await VM.Start();
 
-    // Start up the websocket server
+    // Start up the WebSocket server
     var WS = new WSServer(Config, VM);
     WS.listen();
 }
+
 start();
